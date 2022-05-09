f0c39325fe9b672e7f563eb7976f8e379ac4c66f
It's me, Nik! <72689821+MortalDew@users.noreply.github.com>
2022-05-09 16:48:11 +0300

    Add files via upload

---
+++ task_2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="nik-serializer",
+    version="0.0.10",
+    description="Nik, gruop 053504, format cenverter (Serializer)",
+    author="-",
+    author_email="-",
+    python_requires='>3.8.0',
+    packages=find_packages(),
+    url="https://github.com/MortalDew/ISP-2022-053504/",
+)
